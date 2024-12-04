9ff5bf27d49a826cdd9494e12b94519fefd8d80b
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2024-12-04 12:22:54 +0100

    latest file

--- rename.py
+++ rename.py
@@ -1,4 +1,22 @@
 import glob
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
+def ff():
+    path = os.getenv("PATH_TO_FOLDER")
+    print(path)
+
+
+def recent():
+    folder = os.getenv("PATH_TO_FOLDER")
+    list_of_files = glob.glob(f"{folder}/*")
+
+    latest_file = max(list_of_files, key=os.path.getctime)
+    print(f"Most recent file in {folder} folder is {latest_file}")
+
+
+if __name__ == "__main__":
+    recent()
